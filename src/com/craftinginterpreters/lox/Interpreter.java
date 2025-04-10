a1055779de03607c5d518cfbfc489eed8f1ca2ba
stepperpig <stepperpig@gmail.com>
2025-04-10 14:29:29 -0700

    func returns and closure

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -134,7 +134,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	@Override
 	public Void visitFunctionStmt(Stmt.Function stmt) {
-		LoxFunction function = new LoxFunction(stmt);
+		LoxFunction function = new LoxFunction(stmt, environment);
 		environment.define(stmt.name.lexeme, function);
 		return null;
 	}
@@ -154,6 +154,13 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 		Object value = evaluate(stmt.expression);
 		System.out.println(stringify(value));
 		return null;
+	}
+
+	@Override
+	public Void visitReturnStmt(Stmt.Return stmt) {
+		Object value = null;
+		if (stmt.value != null) value = evaluate(stmt.value);
+		throw new Return(value);
 	}
 
 	@Override
