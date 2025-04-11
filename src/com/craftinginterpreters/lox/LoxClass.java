3b084c0d6ac7e88419c61c8857b63bb64631808c
stepperpig <stepperpig@gmail.com>
2025-04-10 17:12:26 -0700

    whole ass jlox language pretty much

--- src/com/craftinginterpreters/lox/LoxClass.java
+++ src/com/craftinginterpreters/lox/LoxClass.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 class LoxClass implements LoxCallable {
 	final String name;
+	final LoxClass superclass;
 	private final Map<String, LoxFunction> methods;
 
-	LoxClass(String name, Map<String, LoxFunction> methods) {
+	LoxClass(String name, LoxClass superclass, Map<String, LoxFunction> methods) {
+		this.superclass = superclass;
 		this.name = name;
 		this.methods = methods;
 	}
@@ -16,6 +18,11 @@ class LoxClass implements LoxCallable {
 		if (methods.containsKey(name)) {
 			return methods.get(name);
 		}
+
+		if (superclass != null) {
+			return superclass.findMethod(name);
+		}
+
 		return null;
 	}
 
@@ -27,11 +34,18 @@ class LoxClass implements LoxCallable {
 	@Override
 	public Object call(Interpreter interpreter, List<Object> arguments) {
 		LoxInstance instance = new LoxInstance(this);
+		LoxFunction initializer = findMethod("init");
+		if (initializer != null) {
+			initializer.bind(instance).call(interpreter, arguments);
+		}
+
 		return instance;
 	}
 
 	@Override
 	public int arity() {
-		return 0;
+		LoxFunction initializer = findMethod("init");
+		if (initializer == null) return 0;
+		return initializer.arity();
 	}
 }
